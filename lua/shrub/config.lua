1ea59b94c9805412526a17ca8426205710d2a471
Ian Kaplan <ian.kaplan20@gmail.com>
2024-02-28 11:38:39 -0500

    add descriptions to kefault key bindings

--- lua/shrub/config.lua
+++ lua/shrub/config.lua
@@ -7,13 +7,21 @@ M.keys = {
         "n",
         "<leader>jer",
         '<cmd>lua require"shrub".statement_block_surround()<cr>',
-        { noremap = true, silent = true },
+        {
+            noremap = true,
+            silent = true,
+            desc = "intelligent surround with statement block",
+        },
     },
     {
         "n",
         "<leader>jir",
         '<cmd>lua require"shrub".statement_block_surround_undo()<cr>',
-        { noremap = true, silent = true },
+        {
+            noremap = true,
+            silent = true,
+            desc = "intelligent remove surrounding statement block",
+        },
     },
 }
 
